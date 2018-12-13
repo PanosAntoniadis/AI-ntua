19dca81899951248b4122ec1a0b06c9db6515477
PanosAntoniadis <pantoniadis97@gmail.com>
2018-12-13 17:14:43 +0200

    Created Point class and some basic functions

---
+++ Lab1/src/Point.java
@@ -0,0 +1,49 @@
+/**
+ * A class representing a point in the map using its longitude x and its latitude y.
+*/
+public class Point {
+	double x;
+	double y;
+	
+	/**
+	 * @param x
+	 * @param y
+	 */
+	public Point(double x, double y) {
+		this.x = x;
+		this.y = y;
+	}
+
+	/**
+	 * @return the x
+	 */
+	public double getX() {
+		return x;
+	}
+
+	/**
+	 * @param x the x to set
+	 */
+	public void setX(double x) {
+		this.x = x;
+	}
+
+	/**
+	 * @return the y
+	 */
+	public double getY() {
+		return y;
+	}
+
+	/**
+	 * @param y the y to set
+	 */
+	public void setY(double y) {
+		this.y = y;
+	}
+	
+	
+
+	
+}
+

70bfba7d76ba52c31fa9c83b7d09ca3a69df9985
PanosAntoniadis <pantoniadis97@gmail.com>
2018-12-13 18:34:00 +0200

    Read csv files of node and taxis

--- Lab1/src/TaxiBeat.java
+++ Lab1/src/TaxiBeat.java
@@ -7,10 +7,67 @@
 * @author Bazotis Nikos
 * @since   2018-12-18 
 */
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
 
 public class TaxiBeat {
 
 	public static void main(String[] args) {
+		/**
+		 * Define the path of the csv files to read and csv separator (comma).
+		 */
+		String nodesFile = "./nodes.csv";
+		String taxisFile = "./taxis.csv";
+		String clientFile = "./client.csv";
+        String line = "";
+        String cvsSplitBy = ",";
+        String headerLine;
+        String[] fields;
+        /**
+         * Read nodes.csv file and keep all nodes in an arraylist.
+         */
+        try (BufferedReader br = new BufferedReader(new FileReader(nodesFile))) {
+        	headerLine = br.readLine();
+            while ((line = br.readLine()) != null) {
+                /**
+                 * Use comma as separator
+                 */
+                fields = line.split(cvsSplitBy);
+                Street street;
+                if (fields.length == 4) {
+                	street = new Street(Integer.parseInt(fields[2]), fields[3]);
+                }
+                else {
+                	street = new Street(Integer.parseInt(fields[2]));
+                }
+                Node node = new Node(Double.parseDouble(fields[0]), Double.parseDouble(fields[1]), street);
+                Node.nodes.add(node);
+            }
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        
+        /**
+         * Read taxis.csv file and keep all taxis in an arraylist.
+         */
+        try (BufferedReader br = new BufferedReader(new FileReader(taxisFile))) {
+        	headerLine = br.readLine();
+            while ((line = br.readLine()) != null) {
+                /**
+                 * Use comma as separator
+                 */
+                fields = line.split(cvsSplitBy);
+                Taxi taxi = new Taxi(Double.parseDouble(fields[0]), Double.parseDouble(fields[1]), Integer.parseInt(fields[2]));
+                Taxi.taxis.add(taxi);
+            }
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        
+        
 	}
 
 }

ed37b5c92d5b5439d5bd0a674675586f2a9943fe
Panos Antoniadis <vvpp303@gmail.com>
2018-12-13 15:40:17 +0200

    Created class CrossRoad and some getters and setters

---
+++ Lab1/src/CrossRoad.java
@@ -0,0 +1,35 @@
+import java.io.*; 
+import java.util.*; 
+
+/**
+ * A class representing a crossroad using its longitude x, latitude y and the streets that crosses.
+*/
+public class Crossroad extends Point{
+	ArrayList<Street> crossStreets;
+
+	/**
+	 * @param x
+	 * @param y
+	 * @param crossStreets
+	 */
+	public Crossroad(int x, int y, ArrayList<Street> crossStreets) {
+		super(x, y);
+		this.crossStreets = crossStreets;
+	}
+
+	/**
+	 * @return the crossStreets
+	 */
+	public ArrayList<Street> getCrossStreets() {
+		return crossStreets;
+	}
+
+	/**
+	 * @param crossStreets the crossStreets to set
+	 */
+	public void setCrossStreets(ArrayList<Street> crossStreets) {
+		this.crossStreets = crossStreets;
+	}
+	
+	
+}

53f50538a0c4c232fb2ce42444045ec2a6ffb14d
PanosAntoniadis <pantoniadis97@gmail.com>
2018-12-13 17:23:40 +0200

    Just added an empty class that will have the main of the app

---
+++ Lab1/src/TaxiBeat.java
@@ -0,0 +1,16 @@
+/**
+* The Taxibeat program implements an application finds
+* the closest available taxi given as input your geographical
+* location.
+*
+* @author Antoniadis Panagiotis
+* @author Bazotis Nikos
+* @since   2018-12-18 
+*/
+
+public class TaxiBeat {
+
+	public static void main(String[] args) {
+	}
+
+}

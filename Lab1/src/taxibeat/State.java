c39d03e8dd88d3557ee9c2ad1dba66dde3b6be62
PanosAntoniadis <pantoniadis97@gmail.com>
2018-12-22 03:27:05 +0200

    Delete useless methods and add some more comments

--- Lab1/src/taxibeat/State.java
+++ Lab1/src/taxibeat/State.java
@@ -1,5 +1,4 @@
 package taxibeat;
-import java.util.ArrayList;
 /**
  * A class representing a state of the world when running the A* algorithm.
 */
@@ -7,7 +6,6 @@ public class State extends Node{
 	private double distance;
 	private double heuristic;
 	private State previous;
-	private ArrayList<State> myChildren = new ArrayList<State>();
 	
 	/**
 	 * @param x
@@ -17,11 +15,10 @@ public class State extends Node{
 	 * @param heuristic
 	 * @param isGoal
 	 */
-	public State(double x, double y, Street street, double distance, boolean isGoal, State previous) {
-		super(x, y, street, isGoal);
+	public State(double x, double y, Street street, double distance, State previous) {
+		super(x, y, street);
 		this.distance = distance;
 		this.heuristic = computeHeuristic();
-		this.myChildren = null;
 		this.previous = previous;
 	}
 
@@ -53,8 +50,6 @@ public class State extends Node{
 		this.heuristic = heuristic;
 	}
 
-
-	
 	/**
 	 * @return the previous
 	 */
@@ -72,95 +67,13 @@ public class State extends Node{
 	@Override
 	public String toString() {
 		return "State with x = " + getX() + " y = " + getY() + " street id " + getStreet().getStreetId() + " street name " + getStreet().getStreetName() +
-				 " distance = " + distance + ", heuristic = " + heuristic + ", isGoal = " + isGoal() ;
+				 " distance = " + distance + ", heuristic = " + heuristic  ;
 	}
 	
 	/**
-	 * Function that takes a state and returns its children which are the closest nodes
-	 * (including the nodes in other street if we are in a crossing)
-	*/
-	public void setChildren(){
-		ArrayList<State> children = new ArrayList<State>();
-		Point myPoint = new Point(getX(), getY());
-		if (Street.pointCrossings.containsKey(myPoint)){
-			//System.out.println("I am a crosing");
-			for (Street crossingStreet : Street.pointCrossings.get(myPoint)) {
-				ArrayList<Node> currentStreet = Street.streetNodes.get(crossingStreet);
-				int currentIndex = currentStreet.indexOf(new Node(getX(), getY(), crossingStreet, isGoal()));
-				//System.out.println(currentIndex);
-				//System.out.println(Street.pointCrossings.get(myPoint));
-				if (currentIndex < currentStreet.size()-1){
-					Node nextNode = currentStreet.get(currentIndex + 1);
-					//System.out.println(nextNode);
-					double nextNodeDistance = distance + euclideanDistance(this, nextNode);
-					State nextNodeState = new State(nextNode.getX(), nextNode.getY(), nextNode.getStreet(), nextNodeDistance, nextNode.isGoal(), this);
-					children.add(nextNodeState);
-				}
-				if (currentIndex > 0){
-					Node prevNode = currentStreet.get(currentIndex - 1); 
-					//System.out.println(prevNode);
-					double prevNodeDistance = distance + euclideanDistance(this, prevNode);
-					State prevNodeState = new State(prevNode.getX(), prevNode.getY(), prevNode.getStreet(), prevNodeDistance, prevNode.isGoal(), this);
-					children.add(prevNodeState);
-				}
-			}
-		}
-		else {
-			//System.out.println("Im not a crossing with index");
-			ArrayList<Node> myStreet = Street.streetNodes.get(getStreet());
-			//for(int i = 0; i< myStreet.size(); i++) {
-			//	System.out.println(myStreet.get(i));
-			//}
-			int myIndex = myStreet.indexOf(new Node(getX(), getY(), getStreet(), isGoal()));
-			//System.out.println(myIndex);
-			if (myIndex < myStreet.size()-1){
-				Node nextNode = myStreet.get(myIndex + 1);
-				//System.out.println(nextNode);
-				//System.out.println(nextNode.isGoal());
-				double nextNodeDistance = distance + euclideanDistance(this, nextNode);
-				State nextNodeState = new State(nextNode.getX(), nextNode.getY(), nextNode.getStreet(), nextNodeDistance, nextNode.isGoal(), this);
-				children.add(nextNodeState);
-			}
-			if (myIndex > 0){
-				Node prevNode = myStreet.get(myIndex - 1); 
-				//System.out.println(prevNode);
-				//System.out.println(prevNode.isGoal());
-				double prevNodeDistance = distance + euclideanDistance(this, prevNode);
-				State prevNodeState = new State(prevNode.getX(), prevNode.getY(), prevNode.getStreet(), prevNodeDistance, prevNode.isGoal(), this);
-				//System.out.println(prevNodeState.isGoal());
-				children.add(prevNodeState);
-			}
-		}
-		myChildren = children;
-	}
-
-	/**
-	 * @return the myChildren
-	 */
-	public ArrayList<State> getMyChildren() {
-		return myChildren;
-	}
-
-	/**
-	 * @param myChildren the myChildren to set
-	 */
-	public void setMyChildren(ArrayList<State> myChildren) {
-		this.myChildren = myChildren;
-	}
-	
-	/**
-	 * @return the euclidean distance to closest available taxi.
+	 * @return the distance to client.
 	 */
 	public double computeHeuristic() {
-		double minHeuristic = 1000000;
-		for( Taxi taxi : Taxi.taxis) {
-			if (euclideanDistance(taxi, this) < minHeuristic) {
-				minHeuristic = euclideanDistance(taxi, this);
-			}
-		}
-		return minHeuristic;
+		return haversine(getY(), getX(), TaxiBeat.client.getY(), TaxiBeat.client.getX());
 	}
-	
-	
-
 }

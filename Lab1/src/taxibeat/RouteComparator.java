f60742532f96aa0c0a87a9f94922360455c19c5d
PanosAntoniadis <pantoniadis97@gmail.com>
2018-12-22 16:13:12 +0200

    Change the if statements

--- Lab1/src/taxibeat/RouteComparator.java
+++ Lab1/src/taxibeat/RouteComparator.java
@@ -9,10 +9,10 @@ public class RouteComparator implements Comparator<Route> {
 	 *  Overriding compare()method of Comparator  
 	 */
 	public int compare(Route r1, Route r2) { 
-		if (r1.getCost() < r2.getCost()) {
+		if (r1.getCost() > r2.getCost()) {
 			return 1;
 		}
-		else if (r1.getCost() > r2.getCost()) {
+		else if (r1.getCost() < r2.getCost()) {
 			return -1;
 		}
 		return 1;

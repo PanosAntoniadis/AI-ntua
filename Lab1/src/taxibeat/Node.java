2bdc00bacba26e12264540fb8609eb957e4f085e
PanosAntoniadis <pantoniadis97@gmail.com>
2018-12-22 03:20:02 +0200

    Delete some useless methods

--- Lab1/src/taxibeat/Node.java
+++ Lab1/src/taxibeat/Node.java
@@ -5,8 +5,7 @@ import java.util.*;
 */
 public class Node extends Point {
 	private Street street;
-	private boolean available;
-	private boolean isGoal;
+
 	/**
 	 * A list that contains all the nodes in the map.
 	 */
@@ -17,41 +16,9 @@ public class Node extends Point {
 	 * @param y latitude
 	 * @param Street street
 	 */
-	public Node(double x, double y, Street street, boolean isGoal) {
+	public Node(double x, double y, Street street) {
 		super(x, y);
 		this.street = street;
-		this.available = true;
-		this.isGoal = isGoal;
-	}
-
-	/**
-	 * @return the isGoal
-	 */
-	public boolean isGoal() {
-		return isGoal;
-	}
-
-
-	/**
-	 * @param isGoal the isGoal to set
-	 */
-	public void setGoal(boolean isGoal) {
-		this.isGoal = isGoal;
-	}
-
-
-	/**
-	 * @return the available
-	 */
-	public boolean isAvailable() {
-		return available;
-	}
-
-	/**
-	 * @param available the available to set
-	 */
-	public void setAvailable(boolean available) {
-		this.available = available;
 	}
 
 	/**
@@ -70,75 +37,6 @@ public class Node extends Point {
 
 	@Override
 	public String toString() {
-		return "Node with streetId = " + street.getStreetId() + " streetName = " + street.getStreetName() + " x = " + getX() + ", y = " + getY() + " goal " + isGoal;
+		return "Node with streetId = " + street.getStreetId() + " streetName = " + street.getStreetName() + " x = " + getX() + ", y = " + getY();
 	}
-	
-	/**
-	 * 
-	 * @param currentStreet a list containing all the points in the same street
-	 * @return the closest point among the points in the input list
-	 */
-	public Node getClosestNode(ArrayList<Node> currentStreet) {
-		if (currentStreet.size() == 0) {
-			System.out.println("Street is empty");
-			return null;
-		}
-		Node closestNode = null;
-		double minDistance = 1000000;
-		for( Node currentNode : currentStreet) {
-			if ((euclideanDistance(this, currentNode) < minDistance) && !currentNode.equals(this) && currentNode.isAvailable()){
-				minDistance = euclideanDistance(this, currentNode);
-				closestNode = currentNode;
-			}
-		}
-		System.out.println(closestNode);
-		if (closestNode != null) {
-			closestNode.setAvailable(false);
-		}
-		return closestNode;
-	}
-	
-	
-	/**
-	 * @return check if a node is a taxi 
-	 */
-	public boolean isTaxi() {
-		for( Taxi taxi : Taxi.taxis) {
-			if ((taxi.getX() == getX()) && (taxi.getY() == getY())) {
-				return true;
-			}
-		}
-		return false;
-	}
-
-	/* (non-Javadoc)
-	 * @see java.lang.Object#hashCode()
-	 */
-	@Override
-	public int hashCode() {
-		return Objects.hash(available, isGoal, street);
-	}
-
-	/* (non-Javadoc)
-	 * @see java.lang.Object#equals(java.lang.Object)
-	 */
-	@Override
-	public boolean equals(Object obj) {
-		if (this == obj) {
-			return true;
-		}
-		if (!super.equals(obj)) {
-			return false;
-		}
-		if (getClass() != obj.getClass()) {
-			return false;
-		}
-		Node other = (Node) obj;
-		return available == other.available && isGoal == other.isGoal && Objects.equals(street, other.street);
-	}
-
-	
-	
-	
-	
 }

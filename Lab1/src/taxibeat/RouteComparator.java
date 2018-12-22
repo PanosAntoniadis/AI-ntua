c2bf3860a2e22d167db691110e1d5e04ec44ba3c
PanosAntoniadis <pantoniadis97@gmail.com>
2018-12-22 15:00:49 +0200

    Add a comparator for storing routes in an ascending order based on the cost

---
+++ Lab1/src/taxibeat/RouteComparator.java
@@ -0,0 +1,20 @@
+package taxibeat;
+/**
+ * A class that implements the comparator for our tree set based on the cost of each route.
+ */
+import java.util.Comparator;
+
+public class RouteComparator implements Comparator<Route> {
+	/**
+	 *  Overriding compare()method of Comparator  
+	 */
+	public int compare(Route r1, Route r2) { 
+		if (r1.getCost() < r2.getCost()) {
+			return 1;
+		}
+		else if (r1.getCost() > r2.getCost()) {
+			return -1;
+		}
+		return 1;
+	}
+}
